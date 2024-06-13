70ffac4cb26c77d1e151d671076d3d63b0d024c8
ssi-wangshenz@dfmc.com.cn <ssi-wangshenz@dfmc.com.cn>
2024-06-13 10:12:44 +0800

    bugfix

--- ruoyi-ui/src/views/process/modelManagement.vue
+++ ruoyi-ui/src/views/process/modelManagement.vue
@@ -195,7 +195,7 @@ export default {
                     this.getModelByParams(this.searchParams);
                  });
             });
-           
+
         },
         search() {
             this.getModelByParams(this.searchParams);
